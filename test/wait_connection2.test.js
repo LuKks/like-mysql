f62a5540aa9c22b1c2983e70941ed00fa1b37393
LuKks <lucasbarrena@hotmail.com>
2019-11-21 00:28:17 -0300

    added waitConnection

--- test/wait_connection2.test.js
+++ test/wait_connection2.test.js
@@ -4,11 +4,12 @@ const cfg = require('./config.js');
 const db = mysql.createPool(Object.assign(cfg.conn, { connectionLimit: 1, waitForConnections: false }));
 
 (async () => {
+  await db.waitConnection();
+
   let conn = await db.getConnection();
-
   await t.rejects(db.waitConnection());
-
   conn.release();
+  
   await db.end();
   t.pass('');
 })();
